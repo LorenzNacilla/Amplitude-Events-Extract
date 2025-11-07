599f3d8b2955f13544f7ec9e708967a455778a22
LorenzNacilla <lnacilla31@gmail.com>
2025-11-07 14:05:22 +0000

    Adding some logs

--- amplitude/amplitude_extract.py
+++ amplitude/amplitude_extract.py
@@ -2,8 +2,20 @@ import requests
 import os
 import zipfile
 import gzip
+import logging
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+
+log_filename = f"logs/amplitude_extract_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+logging.basicConfig(
+    filemode = "a",
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename=log_filename
+)
+
+logger = logging.getLogger()
 
 # loading the environment that contains our API Keys
 load_dotenv()
@@ -13,7 +25,8 @@ AMP_API_KEY = os.getenv('AMP_API_KEY')
 AMP_SECRET_KEY = os.getenv('AMP_SECRET_KEY')
 
 # setting a start date for our events data can go back x amoubnt of days starting midnight
-start_date = datetime.now() - timedelta(days= 1)
+days_back = 1
+start_date = datetime.now() - timedelta(days = days_back)
 start_date = start_date.strftime('%Y%m%dT00')
 
 # setting an end date for our events data i.e. today
@@ -32,11 +45,18 @@ url = 'https://analytics.eu.amplitude.com/api/2/export'
 
 # calling the API
 response = requests.get(url, params=params, auth=(AMP_API_KEY, AMP_SECRET_KEY))
+logger.info(f"API Call Response Code: '{response.status_code}'")
 
 if response.status_code == 200: # if we get a 200 response (good) then save the data as a zip file
     data = response.content
     print("Data retrieved successfully.")
+    logger.info("Data retrieved successfully.")
+    logger.info("Saving data to data.zip")
     with open('amplitude_data.zip', 'wb') as file:
         file.write(data)
+    logger.info("Data saved to data.zip")
 else:
     print(f'Error {response.status_code}: {response.text}') # otherwise print the status code and the error
+    logger.error(f"API Call Error '{response.status_code}: {response.text}'")
+
+logger.info("Process Finished")
