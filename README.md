dc009c88bc0752d50e3ded2b3363c73841a2ce51
LorenzNacilla <lnacilla31@gmail.com>
2025-11-07 11:07:07 +0000

    Updated README

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 This repo looks into extracting events data from the Amplitude API. There were two methods in calling the API - one using airbyte to call the API itself and then landing it in an azure blob storage, and the other with a python script. This repo mainly contains the python script to call the API but the method with airbyte and azure blob storage is used.
 
 ## ➡️ General Data Flow
+
 ![General Flow](https://github.com/LorenzNacilla/Amplitude-Events-Extract/blob/main/images/General%20Flow.png)
-This is a general flow of the project. For the time being this is looking at more
+This is a general flow of the project. For the time being this is looking at more of the API call and the storing it into a bucket which is more for the airbyte flow shown below.
 
 ![Airbyte Flow](https://github.com/LorenzNacilla/Amplitude-Events-Extract/blob/main/images/Airbyte%20Flow.png)
+This is a diagram that highlights the method of using airbyte and azure blob storage. A big pro of airbyte is that it is no-code and makes use of a UI.
 
 ![Python Flow](https://github.com/LorenzNacilla/Amplitude-Events-Extract/blob/main/images/Python%20Flow.png)
--- amplitude/amplitude_extract.py
+++ amplitude/amplitude_extract.py
@@ -5,35 +5,38 @@ import gzip
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
+# loading the environment that contains our API Keys
 load_dotenv()
 
+# setting and calling variables from our environment that houses the API keys
 AMP_API_KEY = os.getenv('AMP_API_KEY')
 AMP_SECRET_KEY = os.getenv('AMP_SECRET_KEY')
 
+# setting a start date for our events data can go back x amoubnt of days starting midnight
 start_date = datetime.now() - timedelta(days= 1)
 start_date = start_date.strftime('%Y%m%dT00')
-# print(start_date)
-# print(type(start_date))
 
+# setting an end date for our events data i.e. today
 end_date = datetime.now()
 end_date = end_date.strftime('%Y%m%dT23')
-# print(end_date)
-# print(type(start_date))
 
+# this is an argument that will be inserted into our API call to get data for the from our dates set
 params = {
     "start": start_date,
     "end": end_date
 }
 #print(params)
 
+# API url and endpoint
 url = 'https://analytics.eu.amplitude.com/api/2/export'
 
+# calling the API
 response = requests.get(url, params=params, auth=(AMP_API_KEY, AMP_SECRET_KEY))
 
-if response.status_code == 200:
+if response.status_code == 200: # if we get a 200 response (good) then save the data as a zip file
     data = response.content
     print("Data retrieved successfully.")
     with open('amplitude_data.zip', 'wb') as file:
         file.write(data)
 else:
-    print(f'Error {response.status_code}: {response.text}')
+    print(f'Error {response.status_code}: {response.text}') # otherwise print the status code and the error
